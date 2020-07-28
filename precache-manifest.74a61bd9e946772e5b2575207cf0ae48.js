f7bdf3e5841c2fe7a0b7d019ed7a234e0d6ec8c3
AndreaPontrandolfo <andrea.pontrandolfo@nttdata.com>
2020-07-28 16:43:50 +0200

    Updates

---
+++ precache-manifest.74a61bd9e946772e5b2575207cf0ae48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487a8eb039b1c4ea2de82b1b20285e06",
+    "url": "/react-ecommerce-demo/index.html"
+  },
+  {
+    "revision": "4480941faccc8d81b5ab",
+    "url": "/react-ecommerce-demo/static/css/2.3e6a2765.chunk.css"
+  },
+  {
+    "revision": "635266afafec9165fd73",
+    "url": "/react-ecommerce-demo/static/css/main.718ab5b8.chunk.css"
+  },
+  {
+    "revision": "4480941faccc8d81b5ab",
+    "url": "/react-ecommerce-demo/static/js/2.96338525.chunk.js"
+  },
+  {
+    "revision": "6eac990fd58dd12273f8",
+    "url": "/react-ecommerce-demo/static/js/3.c0b8479c.chunk.js"
+  },
+  {
+    "revision": "635266afafec9165fd73",
+    "url": "/react-ecommerce-demo/static/js/main.e8646708.chunk.js"
+  },
+  {
+    "revision": "0ff632718ae6c58bfc20",
+    "url": "/react-ecommerce-demo/static/js/runtime~main.802a068e.js"
+  },
+  {
+    "revision": "5d862cae513110d480cbf13ff064ede6",
+    "url": "/react-ecommerce-demo/static/media/add_to_cart_2.5d862cae.svg"
+  }
+]);
