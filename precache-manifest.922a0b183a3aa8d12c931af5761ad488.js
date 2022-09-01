dad27eaaca621f46715392a987de13c91571e497
AndreaPontrandolfo <andrea.pontra@gmail.com>
2022-09-02 00:13:09 +0200

    Updates

---
+++ precache-manifest.922a0b183a3aa8d12c931af5761ad488.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd51b643f174ad00824ff0ef0326e516",
+    "url": "/react-ecommerce-demo/index.html"
+  },
+  {
+    "revision": "36965e1a4810af929a37",
+    "url": "/react-ecommerce-demo/static/css/2.b27ef7b3.chunk.css"
+  },
+  {
+    "revision": "fefa9540698a881bc554",
+    "url": "/react-ecommerce-demo/static/css/main.e0a6deb9.chunk.css"
+  },
+  {
+    "revision": "36965e1a4810af929a37",
+    "url": "/react-ecommerce-demo/static/js/2.9c1843e1.chunk.js"
+  },
+  {
+    "revision": "c1782b51f9fec7e8beba",
+    "url": "/react-ecommerce-demo/static/js/3.a8f891fe.chunk.js"
+  },
+  {
+    "revision": "fefa9540698a881bc554",
+    "url": "/react-ecommerce-demo/static/js/main.5904289b.chunk.js"
+  },
+  {
+    "revision": "3c4fcf06b8f9aa6a5694",
+    "url": "/react-ecommerce-demo/static/js/runtime~main.b73c57ca.js"
+  },
+  {
+    "revision": "4d8ab944a9623899b5aafd1154972358",
+    "url": "/react-ecommerce-demo/static/media/add_to_cart_2.4d8ab944.svg"
+  }
+]);
